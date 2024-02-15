202989e5f45b935d55c300851fb815544b85b34e
Sergio Diaz <SergioDiazDev@gmail.com>
2024-02-15 19:45:21 +0100

    Para revisar

--- Module06/ex00/ScalarConverter.cpp
+++ Module06/ex00/ScalarConverter.cpp
@@ -1,5 +1,15 @@
 #include "ScalarConverter.hpp"
 
+char ScalarConverter::c = 0;
+int ScalarConverter::i = 0;
+float ScalarConverter::f = 0;
+double ScalarConverter::d = 0;
+char ScalarConverter::type = 0;
+int ScalarConverter::c_err = 0;
+int ScalarConverter::i_err = 0;
+int ScalarConverter::f_err = 0;
+int ScalarConverter::d_err = 0;
+
 ScalarConverter::ScalarConverter()
 {
 }
@@ -19,28 +29,157 @@ ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &copy)
 	return *this;
 }
 
-void ScalarConverter::convert(std::string literal)
+void ScalarConverter::convert(std::string const &literal)
 {
-	std::cout << "Char: " << literal << std::endl;
-	std::cout << "Int: " << getInt(literal) << std::endl;
-	std::cout << "Float: " << getFloat(literal) << std::endl;
-	//std::cout << "double: " << literal << std::endl;
-}
+	char c = getType(literal);	
+	ScalarConverter::type = c;
+	long int temp = strtol(literal.c_str(), NULL, 10);
 
-int			ScalarConverter::getInt(std::string literal)
-{
-	//std::cout << "Char: " << static_cast<int>(literal[0]) << std::endl;
-
-	return (static_cast<int>(literal[0]));
+	switch (c)
+	{
+		case 'c':	
+			ScalarConverter::c = literal.at(0);
+			ScalarConverter::i = static_cast<int>(ScalarConverter::c);
+			ScalarConverter::d = static_cast<double>(ScalarConverter::c);
+			ScalarConverter::f = static_cast<float>(ScalarConverter::c);
+			break;
+		case 'f':
+			errno = 0;
+			ScalarConverter::f = strtof(literal.c_str(), NULL);
+			if (errno == ERANGE || ScalarConverter::f == HUGE_VALF || ScalarConverter::f == -HUGE_VALF)
+				ScalarConverter::f_err = 1;
+			ScalarConverter::c = static_cast<char>(ScalarConverter::f);
+			ScalarConverter::d = static_cast<double>(ScalarConverter::f);
+			ScalarConverter::i = static_cast<int>(ScalarConverter::f);
+			break;
+		case 'i':
+			if (temp > INT_MAX || temp < INT_MIN)
+				ScalarConverter::i_err = 1;
+			ScalarConverter::i = temp;
+			ScalarConverter::c = static_cast<char>(ScalarConverter::i);
+			ScalarConverter::d = static_cast<double>(ScalarConverter::i);
+			ScalarConverter::f = static_cast<float>(ScalarConverter::i);
+			break;
+		case 'd':
+			errno = 0;
+			ScalarConverter::d = strtod(literal.c_str(), NULL);
+			if (errno == ERANGE || ScalarConverter::d == DBL_MAX || ScalarConverter::d == DBL_MIN)
+				ScalarConverter::d_err = 1;
+			ScalarConverter::c = static_cast<char>(ScalarConverter::d);
+			ScalarConverter::i = static_cast<int>(ScalarConverter::d);
+			ScalarConverter::f = static_cast<float>(ScalarConverter::d);
+			break;
+	}
+	ScalarConverter::error();
+	ScalarConverter::print();
 }
-float		ScalarConverter::getFloat(std::string literal)
+char ScalarConverter::getType(std::string const &literal)
 {
-	//std::cout << "Char: " << literal << std::endl;
-	return (static_cast<float>(literal[0]));
-}
-double		ScalarConverter::getDouble(std::string literal)
-{
-	std::cout << "Char: " << literal << std::endl;
+	size_t no = std::string::npos;
+	char type;
 
-	return (1);
+	if (literal.length() == 1 && !isdigit(literal.at(0)))
+		type = 'c';
+	else if (!literal.compare("+inf") || !literal.compare("-inf") || !literal.compare("nan"))
+	{
+		type = 'd';
+		ScalarConverter::i_err = 1;
+		ScalarConverter::c_err = 1;
+	}
+	else if (!literal.compare("+inff") || !literal.compare("-inff") || !literal.compare("nanf"))
+	{
+		type = 'f';
+		ScalarConverter::i_err = 1;
+		ScalarConverter::c_err = 1;
+	}
+	else if (literal.find(".") == no && ScalarConverter::is_number(literal))
+		type = 'i';
+	else if (literal.find("f") == no && literal.find(".") != no)
+		type = 'd';
+	else if (literal.find("f") == literal.length() - 1 && literal.find(".") != no)
+		type = 'f';
+	else
+		type = 'e';
+	return type;
+
+}
+void ScalarConverter::print(void)
+{
+	if (ScalarConverter::c_err || ScalarConverter::type == 'e')
+		std::cout << "char: impossible" << std::endl;
+	else if (ScalarConverter::c > 31 && ScalarConverter::c < 127)
+		std::cout << "char: '" << ScalarConverter::c << "'"<< std::endl;
+	else
+		std::cout << "char: Non displayable" << std::endl;
+	if (ScalarConverter::i_err || ScalarConverter::type == 'e')
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << ScalarConverter::i << std::endl;
+	if (ScalarConverter::f_err || ScalarConverter::type == 'e')
+		std::cout << "float: impossible" << std::endl;
+	else
+	{
+		std::cout << "float: ";
+		if (ScalarConverter::f - static_cast<int>(ScalarConverter::f) == 0)
+			std::cout << ScalarConverter::f << ".0f" << std::endl;
+		else
+			std::cout << ScalarConverter::f << "f" << std::endl;
+	}
+	if (ScalarConverter::d_err || ScalarConverter::type == 'e')
+		std::cout << "double: impossible" << std::endl;
+	else
+	{
+		std::cout << "double: ";
+		if (ScalarConverter::d - static_cast<int>(ScalarConverter::d) == 0)
+			std::cout << ScalarConverter::d << ".0" << std::endl;
+		else
+			std::cout << ScalarConverter::d << std::endl;
+	}
+}
+void ScalarConverter::error(void)
+{
+	if (ScalarConverter::d < -128 || ScalarConverter::d > 127 || ScalarConverter::i < -128 \
+		|| ScalarConverter::i > 127 || ScalarConverter::f < -128 || ScalarConverter::f > 127)
+		ScalarConverter::c_err = 1;
+	if (ScalarConverter::type == 'i' && ScalarConverter::i_err)
+	{
+		ScalarConverter::d_err = 1;
+		ScalarConverter::f_err = 1;
+		ScalarConverter::c_err = 1;
+	}
+ 	if (ScalarConverter::type == 'f')
+	{
+		if (ScalarConverter::f <= static_cast<float>(std::numeric_limits<int>::min()) || \
+			ScalarConverter::f >= static_cast<float>(std::numeric_limits<int>::max()))
+			ScalarConverter::i_err = 1;
+		if (ScalarConverter::f_err)
+		{
+			ScalarConverter::d_err = 1;
+			ScalarConverter::i_err = 1;
+			ScalarConverter::c_err = 1;
+		}
+	}
+	if (ScalarConverter::type == 'd')
+	{
+		if (ScalarConverter::d < static_cast<double>(std::numeric_limits<int>::min()) \
+			|| ScalarConverter::d > static_cast<double>(std::numeric_limits<int>::max()))
+			ScalarConverter::i_err = 1;
+		if (ScalarConverter::d_err)
+		{
+			ScalarConverter::f_err = 1;
+			ScalarConverter::i_err = 1;
+			ScalarConverter::c_err = 1;
+		}
+	}
+}
+bool ScalarConverter::is_number(std::string const &literal)
+{
+	for (size_t i = 0; i < literal.length(); i++)
+	{
+		if (literal[i] == '-' && i == 0)
+			continue;
+		else if (!std::isdigit(literal[i]))
+			return false;
+	}
+	return true;
 }
--- Module06/ex00/ScalarConverter.hpp
+++ Module06/ex00/ScalarConverter.hpp
@@ -2,8 +2,13 @@
 #define SCALARCONVERTER_HPP
 
 # include <iostream>
-# include <cstdlib>
-#include <string>
+# include <string>
+# include <exception>
+# include <errno.h>
+# include <limits>
+# include <limits.h>
+# include <float.h>
+# include <math.h>
 
 class ScalarConverter
 {
@@ -12,13 +17,24 @@ class ScalarConverter
 		ScalarConverter(ScalarConverter const &copy);
 		ScalarConverter	&operator=(ScalarConverter const &copy);
 
+		static char c;
+		static int i;
+		static float f;
+		static double d;
+		static char type;
+		static int c_err;
+		static int i_err;
+		static int f_err;
+		static int d_err;
+
 	public:
 		~ScalarConverter();
 
-		static void		convert(std::string literal);
-		static int			getInt(std::string literal);
-		static float		getFloat(std::string literal);
-		static double		getDouble(std::string literal);
+		static void convert(std::string const &literal);
+		static char getType(std::string const &literal);
+		static void print(void);
+		static void error(void);
+		static bool is_number(std::string const &literal);
 
 };
 
